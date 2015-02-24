091e942d9d318a2130a69afbab3ec6ba898c7ad3
Steve Wampler <steve@blacklocus.com>
2015-02-24 13:54:48 -0600

    cleaned up eight knights

--- src/main/scala/com/sgw/problems/EightKnights.scala
+++ src/main/scala/com/sgw/problems/EightKnights.scala
@@ -21,47 +21,48 @@ object EightKnights {
   )
 
   case class Knight(id: Int, n: Int, row: Int, col: Int) {
-    def advance(): Option[Knight] = {
+    def advance(): Knight = {
       if (row + 1 >= n) {
-        if (col + 1 >= n) {
-          None
-        } else {
-          Some(copy(row = 0, col = col + 1))
-        }
+        copy(row = 0, col = col + 1)
       } else {
-        Some(copy(row = row + 1))
+        copy(row = row + 1)
       }
     }
+
+    def isOffBoard: Boolean = col >= n
   }
 
+  @tailrec
   private def go(n: Int, knight: Knight, solution: List[Knight], solutions: List[List[Knight]]): List[List[Knight]] = {
-    // if the knight is checked ...
+    // we stop the recursion whenever a knight is advanced off of the right side of the board
+    if (knight.isOffBoard) return solutions
+
+    // get the current list of solutions, advance the knight, and recurse to find for more solutions
+    go(n, knight.advance(), solution, getSolutions(n, knight, solution, solutions))
+  }
+
+  private def getSolutions(n: Int, knight: Knight, solution: List[Knight], solutions: List[List[Knight]]): List[List[Knight]] =
+    // if the knight is checked ..
     if (inCheck(knight, solution)) {
-      // move the knight if possible, otherwise return the current solutions
-      knight.advance().map(k => go(n, k, solution, solutions)).getOrElse(solutions)
-    } else {
-      // a potential (and perhaps partial) solution
+      // nothing new to add, so just return the current list of solutions
+      solutions
+    } else { // knight is not checked, so ...
+      // add the knight the solution
       val newSolution = knight :: solution
 
       // if we've found a complete solution ...
       if (newSolution.size == n) {
-        // add it to the current solutions
-        val newSolutions = newSolution :: solutions
-
-        // advance the knight if possible, otherwise return the current solutions
-        knight.advance().map(k => go(n, k, solution, newSolutions)).getOrElse(newSolutions)
+        // add the solution to the list of solutions
+        newSolution :: solutions
       } else {
-        // create a new knight at the same position as the current knight
+        // otherwise; we don't have a complete solution, so
+        // create a new knight at the same position as the current knight and ...
         val newKnight = knight.copy(knight.id + 1)
 
-        // advance the new knight if possible; otherwise return the current solutions
-        val newSolutions = newKnight.advance().map(k => go(n, k, newSolution, solutions)).getOrElse(solutions)
-
-        // advance the current knight if possible looking for additional solutions; otherwise return the new solutions
-        knight.advance().map(k => go(n, k, solution, newSolutions)).getOrElse(newSolutions)
+        // advance the new knight and recurse to find for more solutions
+        go(n, newKnight.advance(), newSolution, solutions)
       }
     }
-  }
 
   private def inCheck(knight: Knight, solution: List[Knight]): Boolean = OFFSETS.map {
     case (rowOffset, colOffset) => (knight.row + rowOffset, knight.col + colOffset)
