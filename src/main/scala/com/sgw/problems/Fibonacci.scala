5d170b8adb13a232102004d246b9c709e6f1a46f
wamplers <steve.wampler@avention.com>
2015-09-16 07:06:07 -0500

    Some more fibonacci alternatives.

--- src/main/scala/com/sgw/problems/Fibonacci.scala
+++ src/main/scala/com/sgw/problems/Fibonacci.scala
@@ -8,7 +8,7 @@ import scala.annotation.tailrec
 object Fibonacci {
   @tailrec
   private def fibonacci(n: Int, seq: List[Int]): List[Int] = {
-    if (n <= 2) return seq.reverse
+    if (n <= 1) return seq.reverse
 
     fibonacci(n - 1, (seq.head + seq.tail.head) :: seq)
   }
@@ -17,9 +17,8 @@ object Fibonacci {
 
   // using foldLeft
   def fibonacci2(n: Int): List[Int] =
-    (0 until n - 2).foldLeft(List(1, 0))((z, _) => (z.head + z.tail.head) :: z).reverse
+    (0 until n - 1).foldLeft(List(1, 0))((z, _) => (z.head + z.tail.head) :: z).reverse
 
-  // hmmmm
   def fibonacci3(n: Int, result: List[Int] = List[Int]()): List[Int] = {
     n match {
       case 0 => 0 :: result
@@ -31,9 +30,42 @@ object Fibonacci {
     }
   }
 
+  @tailrec
+  def fib(i: Int, prev: Int = 1, prevprev: Int = 0): Int = {
+    if (i < 0) {
+      throw new RuntimeException("i must be >= 0")
+    } else if (i == 0 || i == 1) {
+      i
+    } else if (i == 2)
+      prev + prevprev
+    else
+      fib(i - 1, prev + prevprev, prev)
+  }
+
+  def fib2(i: Int): Int = {
+    var n = i
+
+    var p1 = 1
+    var p2 = 0
+
+    while(n > 1) {
+      val tmp = p1 + p2
+
+      p2 = p1
+
+      p1 = tmp
+
+      n = n - 1
+    }
+
+    p1
+  }
+
   def main (args: Array[String]) {
     println(fibonacci(11))
     println(fibonacci2(11))
     println(fibonacci3(11).reverse)
+    println(fib(11))
+    println(fib2(11))
   }
 }
