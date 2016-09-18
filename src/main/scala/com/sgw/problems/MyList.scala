66e75e962f2030c9e7bec07b65c8094bbd489ec6
stevewampler <swampler@spredfast.com>
2016-09-18 18:16:45 -0500

    Added a few methods on my list class.

--- src/main/scala/com/sgw/problems/MyList.scala
+++ src/main/scala/com/sgw/problems/MyList.scala
@@ -1,5 +1,7 @@
 package com.sgw.problems
 
+import math.Ordering
+
 object MyList {
   def apply[T](): MyList[T] = MyNil
 
@@ -21,6 +23,19 @@ trait MyList[+T] {
 
   def head: T
   def tail: MyList[T]
+  def last: T
+  def init: MyList[T]
+
+  def take(n: Int): MyList[T]
+  def drop(n: Int): MyList[T]
+
+  def concat[E >: T](list: MyList[E]): MyList[E]
+
+  def flatten: MyList[Any]
+
+  def removeAt(n: Int): MyList[T]
+
+  def length: Int
 
   def apply(i: Int): T
 
@@ -32,24 +47,47 @@ trait MyList[+T] {
 
   def ::[E >: T](elem: E): MyList[E] = MyCons(elem, this)
 
-  def sort(lt: (T, T) => Boolean): MyList[T]
+  def :::[E >: T](list: MyList[E]): MyList[E] = concat(list)
 
-  def insertSorted[E >: T](x: E, lt: (E, T) => Boolean): MyList[E]
+  def splitAt(n: Int): (MyList[T], MyList[T])
+
+  def insertionSort[E >: T](implicit lt: (E, T) => Boolean): MyList[E]
+
+  def insertSorted[E >: T](x: E)(implicit lt: (E, T) => Boolean): MyList[E]
+
+  def mergeSort(lt: (T, T) => Boolean): MyList[T]
 }
 
 object MyNil extends MyList[Nothing] {
   def isEmpty: Boolean = true
 
   def head: Nothing = throw new NoSuchElementException("empty list")
-  def tail: Nothing = throw new NoSuchElementException("empty list")
+  def tail: MyList[Nothing] = throw new NoSuchElementException("empty list")
+  def last: Nothing = throw new NoSuchElementException("empty list")
+  def init: MyList[Nothing] = throw new NoSuchElementException("empty list")
+
+  def concat[B >: Nothing](ys: MyList[B]): MyList[B] = ys
+
+  def take(n: Int): MyList[Nothing] = this
+  def drop(n: Int): MyList[Nothing] = this
+
+  def length: Int = 0
+
+  def removeAt(n: Int): MyList[Nothing] = MyNil
+
+  def flatten: MyList[Nothing] = this
 
   def foldLeft[B](z: B)(f: (B, Nothing) => B): B = z
 
+  def splitAt(n: Int): (MyList[Nothing], MyList[Nothing]) = (MyNil, MyNil)
+
   def apply(i: Int): Nothing = throw new IndexOutOfBoundsException
 
-  def sort(lt: (Nothing, Nothing) => Boolean): MyList[Nothing] = MyNil
+  def insertionSort[E >: Nothing](implicit lt: (E, Nothing) => Boolean): MyList[E] = MyNil
 
-  def insertSorted[E >: Nothing](x: E, lt: (E, Nothing) => Boolean): MyList[E] = x :: MyNil
+  def insertSorted[E >: Nothing](x: E)(implicit lt: (E, Nothing) => Boolean): MyList[E] = x :: MyNil
+
+  def mergeSort(lt: (Nothing, Nothing) => Boolean): MyList[Nothing] = MyNil
 
   override def toString = ""
 }
@@ -57,18 +95,79 @@ object MyNil extends MyList[Nothing] {
 case class MyCons[+T](head: T, tail: MyList[T] = MyNil) extends MyList[T] {
   def isEmpty: Boolean = false
 
+  def last: T = tail match {
+    case MyNil => head
+    case _ => tail.last
+  }
+
+  def init: MyList[T] = tail match {
+    case MyNil => MyNil
+    case _ => MyCons(head, tail.init)
+  }
+
+  def concat[B >: T](ys: MyList[B]): MyList[B] = ys match {
+    case MyNil => this
+    case _ => MyCons(ys.head, concat(ys.tail))
+  }
+
+  def flatten: MyList[Any] = head match {
+    case xs: MyList[_] => tail.flatten.concat(xs.flatten)
+    case _ => MyCons(head, tail.flatten)
+  }
+
+  def take(n: Int): MyList[T] =
+    if (n <= 0) MyNil
+    else MyCons(head, tail.take(n - 1))
+
+  def drop(n: Int): MyList[T] =
+    if (n <= 0) this
+    else tail.drop(n - 1)
+
+  def removeAt(n: Int): MyList[T] =
+    if (n < 0)
+      this
+    else if (n == 0)
+      tail
+    else
+      MyCons(head, tail.removeAt(n - 1))
+
+  def length: Int = 1 + tail.length
+
   def foldLeft[B](z: B)(f: (B, T) => B): B = tail.foldLeft(f(z, head))(f)
 
   def apply(i: Int): T = if (i == 0) head else tail.apply(i - 1)
 
   // complexity is N * N because, in the worst case, we have to visit all N values and then insert all N values
-  def sort(lt: (T, T) => Boolean): MyList[T] =
-    tail.sort(lt).insertSorted(head, lt)
+  def insertionSort[E >: T](implicit lt: (E, T) => Boolean): MyList[E] = tail.insertionSort.insertSorted(head)
 
-  def insertSorted[E >: T](x: E, lt: (E, T) => Boolean): MyList[E] = if (x == head || lt(x, head)) {
+  def splitAt(n: Int): (MyList[T], MyList[T]) = (take(n), drop(n))
+
+  def insertSorted[E >: T](x: E)(implicit lt: (E, T) => Boolean): MyList[E] = if (x == head || lt(x, head)) {
     x :: this
   } else {
-    head :: tail.insertSorted(x, lt)
+    head :: tail.insertSorted(x)
+  }
+
+  def mergeSort(lt: (T, T) => Boolean): MyList[T] = {
+    val n = length / 2
+
+    if (n == 0)
+      this
+    else {
+      def merge(left: MyList[T], right: MyList[T]): MyList[T] = (left, right) match {
+        case (_, MyNil) => left
+        case (MyNil, _) => right
+        case (MyCons(leftHead, leftTail), MyCons(rightHead, rightTail)) => if (lt(leftHead, rightHead)) {
+          leftHead :: merge(leftTail, right)
+        } else {
+          rightHead :: merge(left, rightTail)
+        }
+      }
+
+      val (left, right) = splitAt(n)
+
+      merge(left.mergeSort(lt), right.mergeSort(lt))
+    }
   }
 
   override def toString = s"$head${if (tail != MyNil) "," else ""}$tail"
--- src/test/scala/com/sgw/problems/MyListSpec.scala
+++ src/test/scala/com/sgw/problems/MyListSpec.scala
@@ -33,10 +33,70 @@ class MyListSpec extends FlatSpec with Matchers {
     MyList(1, 2, 3) should be (MyList(1, MyList(2, MyList(3))))
   }
 
-  "A list" should "be able to be sorted" in {
+  "The last of a list" should "be the last element of the list" in {
     val list = MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3)
-    println(list)
-    val sorted = list.sort((x, y) => x < y)
-    println(sorted)
+    list.last should be (3)
+  }
+
+  "The init of a list" should "be all but the last element" in {
+    val list = MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3)
+    list.init should be (MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19))
+  }
+
+  "Taking n elements of a list" should "return a list of the first n elements" in {
+    MyList().take(5) should be (MyList())
+    MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3).take(5) should be (MyList(5, 2, 1, 6, 8))
+  }
+
+  "Dropping n elements of a list" should "return a list without the first n elements" in {
+    MyList().drop(5) should be (MyList())
+    MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3).drop(5) should be (MyList(10, 50, 24, 20, 19, 3))
+  }
+
+  "Splitting a list at the nth elements" should "return two lists" in {
+    MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3).splitAt(5) should be (MyList(5, 2, 1, 6, 8), MyList(10, 50, 24, 20, 19, 3))
+  }
+
+  "The removeAt method" should "remove the ith element of a list" in {
+    val list = MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3)
+
+    list.removeAt(-5) should be (list)
+    list.removeAt(0) should be (list.tail)
+    list.removeAt(5) should be (MyList(5, 2, 1, 6, 8, 50, 24, 20, 19, 3))
+    list.removeAt(10) should be (list.init)
+    list.removeAt(11) should be (list)
+  }
+
+  "Concatenating two lists" should "concatenate the lists" in {
+    val list1 = MyList(1, 2, 3)
+    val list2 = MyList(4, 5, 6)
+
+    list2.concat(list1) should be (MyList(1,2,3,4,5,6))
+
+    list1 ::: list2 should be (MyList(1,2,3,4,5,6))
+  }
+
+  "Flattening a list" should "flatten the list" in {
+    val list = MyList(1, MyList(2, 3), 4, MyList(5, 6, MyList(7, 8)))
+
+    list.flatten should be (MyList(1, 2, 3, 4, 5, 6, 7, 8))
+  }
+
+  "The length of a list" should "be the length of the list" in {
+    MyList().length should be (0)
+
+    MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3).length should be (11)
+  }
+
+  "A list" should "be able to be sorted using an insertion sort" in {
+    val list = MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3)
+
+    list.insertionSort((x, y) => x < y) should be (MyList(1,2,3,5,6,8,10,19,20,24,50))
+  }
+
+  "A list" should "be able to be sorted using a merge sort" in {
+    val list = MyList(5, 2, 1, 6, 8, 10, 50, 24, 20, 19, 3)
+
+    list.mergeSort((x, y) => x < y) should be (MyList(1,2,3,5,6,8,10,19,20,24,50))
   }
 }
